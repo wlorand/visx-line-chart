1afe0275df46aea8655445cea47e04cf4a089f3e
Will Lorand <will@milemarker3.org>
2020-10-13 14:57:48 -0700

    stub of line chart temps

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 
+// child components
+import LineChartTemps from './components/LineChartTemps';
+
+// styles
 import './app.css';
 
 function App() {
   return (
     <div>
-     <h1 className="app-title">Visx Line Chart</h1>
-     <p className="app-subhead"> note: axis and appl stock data coming soon </p>
+      <h1 className="app-title">Visx Line Chart</h1>
+      <p className="app-subhead">note: axis and appl stock data coming soon</p>
+      <LineChartTemps />
     </div>
   );
 }
---
+++ src/components/LineChartTemps.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// @visx imports
+
+function LineChartTease() {
+  return (
+    <div>
+      <p>i am line chart temps</p>
+    </div>
+  );
+}
+
+export default LineChartTease;
